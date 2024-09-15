8b65d95fc290c940ee565b18f3934a99354c060b
ImKetan1610 <ketan.makode@gmail.com>
2024-09-15 07:37:43 +0530

    craeted a stactic grid like structure for the visble part of the screen

--- src/Components/Sheets.tsx
+++ src/Components/Sheets.tsx
@@ -1,10 +1,86 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import {
+  calculateRowsAndColToDisplay,
+  resizeCanvas,
+} from "../utils/Sheet.utils";
 
 export const Sheets = () => {
-  return (
-    <div>
-      <h1>Sheets</h1>
-    </div>
-  )
-}
+  const canvasRef = useRef(null);
 
+  const canvasWidth = window.innerWidth;
+  const canvasHeight = window.innerHeight;
+
+  const cellWidth = 100;
+  const cellHeight = 22;
+
+  const {
+    visible: visibleColumns,
+    start: columnsStart,
+    end: columnsEnd,
+  } = calculateRowsAndColToDisplay(cellWidth, canvasWidth);
+  const {
+    visible: visibleRows,
+    start: rowsStart,
+    end: rowsEnd,
+  } = calculateRowsAndColToDisplay(cellHeight, canvasHeight);
+  // console.log(visibleColumns, visibleRows);
+  // console.log(columnsStart, columnsEnd);
+  // console.log(rowsStart, rowsEnd);
+
+  useEffect(() => {
+    // first we need to take the reference of the canvas to draw something on it
+    let canvas = canvasRef.current;
+    resizeCanvas(canvas);
+    // then we need to select the tool of canvas to draw
+    const context = canvas.getContext("2d");
+
+    // create a complete rectangle for the canvas and added white color to it
+    context.fillStyle = "white";
+    // added dimension for the rectangle
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+    // draw col lines
+    let startX = 0;
+
+    // to draw vertical lines we need to write the for loop over the visibleColumns array
+    for (let col of visibleColumns) {
+      context.beginPath();
+      // where to start drawing the lines
+      context.moveTo(startX, 0);
+      // where to end
+      context.lineTo(startX, context.canvas.height);
+      // Draw line
+      context.stroke();
+
+      startX += cellWidth;
+    }
+
+    let startY = 0;
+
+    // to draw horizontal lines we need to write the for loop over the visibleRows array
+    for (let row of visibleRows) {
+      context.beginPath();
+      // where to start drawing the lines
+      context.moveTo(0, startY);
+      // where to end
+      context.lineTo(context.canvas.width, startY);
+      // Draw line
+      context.stroke();
+
+      startY += cellHeight;
+    }
+  }, []);
+
+  return (
+    <div style={{ height: "100vh", width: "100vw", backgroundColor: "red" }}>
+      <canvas
+        ref={canvasRef}
+        style={{
+          height: canvasHeight,
+          width: canvasWidth,
+          backgroundColor: "yellow",
+        }}
+      />
+    </div>
+  );
+};
---
+++ src/utils/Sheet.utils.ts
@@ -0,0 +1,51 @@
+// function for calculating the no of visible rows and cols
+export const calculateRowsAndColToDisplay = (size, visibleArea) => {
+  // initialize an array to store the col or row
+  const visible = [];
+
+  // need to initialize two more arrays for having the start and end pointers of the rows and cols
+  const start = [];
+  const end = [];
+
+  //  initializing a variable from where the col or row starts
+  let idx = 0;
+  let nextStart = 0;
+
+  //  creating a loop to check where the col can be added inside the visible area or not
+  while (nextStart < visibleArea) {
+    // we need to add the col in the visible array
+    visible.push(idx);
+    start.push(nextStart);
+    end.push(nextStart + size);
+
+    idx++;
+    // new nextStart of col or row can be get by adding the size of cell
+    nextStart += size;
+  }
+
+  // and at the end we need to return the visible array.
+  return { visible, start, end };
+};
+
+// we need to resize the canvas or else the drawn lines are not visible as per our device
+export const resizeCanvas = (canvas) => {
+  // getBoundingClientRect is used to get size and position of an element relative to its visible part of the web page
+  const { width, height } = canvas.getBoundingClientRect();
+
+  // we also need to rescale the width as per the ratio of the machine
+  //  thus we need to calculate the device pixel ratio
+  let ratio = window.devicePixelRatio;
+
+  let newCanvasWidth = Math.round(width * ratio);
+  let newCanvasHeight = Math.round(height * ratio);
+
+  // obtain a 2D rendering context
+  // need a tool for drawing
+  let context = canvas.getContext("2d");
+  canvas.width = newCanvasWidth;
+  canvas.height = newCanvasHeight;
+
+  // we need to multiply the current transformation matrix by a scaling matrix
+  // which allow us to enlarge or shrink everything drawn on the canvas.
+  context.scale(ratio, ratio);
+};
